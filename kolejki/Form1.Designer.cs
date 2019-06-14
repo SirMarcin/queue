e40349be4401c375f162741d088384f17045bb1f
Marcin Webmaster <buczynski.programuje@gmail.com>
2019-06-14 16:32:43 +0200

    init

--- kolejki/Form1.Designer.cs
+++ kolejki/Form1.Designer.cs
@@ -28,13 +28,138 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.makeTaskButton = new System.Windows.Forms.Button();
+            this.currentTaskTextBox = new System.Windows.Forms.TextBox();
+            this.highPriorityList = new System.Windows.Forms.TextBox();
+            this.lowPriorityList = new System.Windows.Forms.TextBox();
+            this.highPriorityLabel = new System.Windows.Forms.Label();
+            this.lowPriorityLabel = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.finishedTasksList = new System.Windows.Forms.TextBox();
+            this.finishedTasksLLabel = new System.Windows.Forms.Label();
+            this.currentTaskLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // makeTaskButton
+            // 
+            this.makeTaskButton.Location = new System.Drawing.Point(15, 334);
+            this.makeTaskButton.Name = "makeTaskButton";
+            this.makeTaskButton.Size = new System.Drawing.Size(206, 29);
+            this.makeTaskButton.TabIndex = 0;
+            this.makeTaskButton.Text = "Make new task";
+            this.makeTaskButton.UseVisualStyleBackColor = true;
+            this.makeTaskButton.Click += new System.EventHandler(this.makeTaskButton_Click);
+            // 
+            // currentTaskTextBox
+            // 
+            this.currentTaskTextBox.Location = new System.Drawing.Point(15, 144);
+            this.currentTaskTextBox.Name = "currentTaskTextBox";
+            this.currentTaskTextBox.Size = new System.Drawing.Size(205, 20);
+            this.currentTaskTextBox.TabIndex = 1;
+            // 
+            // highPriorityList
+            // 
+            this.highPriorityList.Location = new System.Drawing.Point(15, 212);
+            this.highPriorityList.Multiline = true;
+            this.highPriorityList.Name = "highPriorityList";
+            this.highPriorityList.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.highPriorityList.Size = new System.Drawing.Size(100, 116);
+            this.highPriorityList.TabIndex = 2;
+            // 
+            // lowPriorityList
+            // 
+            this.lowPriorityList.Location = new System.Drawing.Point(121, 212);
+            this.lowPriorityList.Multiline = true;
+            this.lowPriorityList.Name = "lowPriorityList";
+            this.lowPriorityList.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.lowPriorityList.Size = new System.Drawing.Size(100, 116);
+            this.lowPriorityList.TabIndex = 3;
+            // 
+            // highPriorityLabel
+            // 
+            this.highPriorityLabel.AutoSize = true;
+            this.highPriorityLabel.Location = new System.Drawing.Point(12, 196);
+            this.highPriorityLabel.Name = "highPriorityLabel";
+            this.highPriorityLabel.Size = new System.Drawing.Size(62, 13);
+            this.highPriorityLabel.TabIndex = 4;
+            this.highPriorityLabel.Text = "High priority";
+            // 
+            // lowPriorityLabel
+            // 
+            this.lowPriorityLabel.AutoSize = true;
+            this.lowPriorityLabel.Location = new System.Drawing.Point(118, 196);
+            this.lowPriorityLabel.Name = "lowPriorityLabel";
+            this.lowPriorityLabel.Size = new System.Drawing.Size(60, 13);
+            this.lowPriorityLabel.TabIndex = 5;
+            this.lowPriorityLabel.Text = "Low priority";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(15, 170);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(205, 23);
+            this.progressBar.TabIndex = 6;
+            // 
+            // finishedTasksList
+            // 
+            this.finishedTasksList.Location = new System.Drawing.Point(15, 25);
+            this.finishedTasksList.Multiline = true;
+            this.finishedTasksList.Name = "finishedTasksList";
+            this.finishedTasksList.Size = new System.Drawing.Size(205, 96);
+            this.finishedTasksList.TabIndex = 7;
+            // 
+            // finishedTasksLLabel
+            // 
+            this.finishedTasksLLabel.AutoSize = true;
+            this.finishedTasksLLabel.Location = new System.Drawing.Point(12, 9);
+            this.finishedTasksLLabel.Name = "finishedTasksLLabel";
+            this.finishedTasksLLabel.Size = new System.Drawing.Size(74, 13);
+            this.finishedTasksLLabel.TabIndex = 8;
+            this.finishedTasksLLabel.Text = "Finished tasks";
+            // 
+            // currentTaskLabel
+            // 
+            this.currentTaskLabel.AutoSize = true;
+            this.currentTaskLabel.Location = new System.Drawing.Point(12, 128);
+            this.currentTaskLabel.Name = "currentTaskLabel";
+            this.currentTaskLabel.Size = new System.Drawing.Size(64, 13);
+            this.currentTaskLabel.TabIndex = 9;
+            this.currentTaskLabel.Text = "Current task";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(232, 379);
+            this.Controls.Add(this.currentTaskLabel);
+            this.Controls.Add(this.finishedTasksLLabel);
+            this.Controls.Add(this.finishedTasksList);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.lowPriorityLabel);
+            this.Controls.Add(this.highPriorityLabel);
+            this.Controls.Add(this.lowPriorityList);
+            this.Controls.Add(this.highPriorityList);
+            this.Controls.Add(this.currentTaskTextBox);
+            this.Controls.Add(this.makeTaskButton);
+            this.Name = "Form1";
+            this.Text = "Dwukolejka";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button makeTaskButton;
+        private System.Windows.Forms.TextBox currentTaskTextBox;
+        private System.Windows.Forms.TextBox highPriorityList;
+        private System.Windows.Forms.TextBox lowPriorityList;
+        private System.Windows.Forms.Label highPriorityLabel;
+        private System.Windows.Forms.Label lowPriorityLabel;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.TextBox finishedTasksList;
+        private System.Windows.Forms.Label finishedTasksLLabel;
+        private System.Windows.Forms.Label currentTaskLabel;
     }
 }
 
--- kolejki/Form1.cs
+++ kolejki/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +13,95 @@ namespace kolejki
 {
     public partial class Form1 : Form
     {
+        Random rnd = new Random();
+        Queue<Task> highQueue = new Queue<Task>();
+        Queue<Task> lowQueue = new Queue<Task>();
+
+        
         public Form1()
         {
             InitializeComponent();
         }
+        
+        public void makeTaskButton_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Task t = new Task(() =>
+                {
+                    Thread.Sleep(rnd.Next(500, 1000));
+                });
+
+                int p = rnd.Next(0, 2);
+                if (p == 0)
+                {
+                    highQueue.Enqueue(t);
+                }
+                else
+                {
+                    lowQueue.Enqueue(t);
+                }
+            }
+            
+
+            highPriorityList.Text = "";
+            lowPriorityList.Text = "";
+
+            foreach (var t in highQueue)
+            {
+                highPriorityList.Text += $"Task #{t.Id}";
+                highPriorityList.Text += Environment.NewLine;
+            }
+
+            foreach (var t in lowQueue)
+            {
+                lowPriorityList.Text += $"Task #{t.Id}";
+                lowPriorityList.Text += Environment.NewLine;
+            }
+            try
+            {
+                foreach (var item in highQueue)
+                {
+                    highPriorityList.Text = "";
+                    foreach (var t in highQueue)
+                    {
+                        highPriorityList.Text += $"Task #{t.Id}";
+                        highPriorityList.Text += Environment.NewLine;
+                    }
+                    item.Start();
+                    currentTaskTextBox.Text = $"Task #{item.Id}:";
+                    currentTaskTextBox.Text += item.Status;
+                    item.Wait();
+                    highQueue.Dequeue();
+                }
+
+                if (highQueue.Count == 0)
+                {
+                    foreach (var item in lowQueue)
+                    {
+                        lowPriorityList.Text = "";
+                        foreach (var t in lowQueue)
+                        {
+                            lowPriorityList.Text += $"Task #{t.Id}";
+                            lowPriorityList.Text += Environment.NewLine;
+                        }
+                        item.Start();
+                        item.Wait();
+                        currentTaskTextBox.Text = $"Task #{item.Id}:";
+                        currentTaskTextBox.Text += item.Status;
+                        lowQueue.Dequeue();
+                    }
+                }
+
+            }
+            catch (Exception)
+            {
+
+            }
+
+
+        }
+        
+
     }
 }
